f7955ed02c270c0fb93b0cd3f3d684a263017a7b
pls <124419933+plsuwu@users.noreply.github.com>
2024-08-04 17:01:29 +1000

    Update coin-logic.tsx

--- src/lib/socket/coin-logic.tsx
+++ src/lib/socket/coin-logic.tsx
@@ -1,11 +1,5 @@
 
 // this doesnt have to be super complex so we just fuck
 export const fiftyfifty = () => {
-    let res = Math.random();
-
-    if (res > 0.5) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return Math.random() >= 0.5 ? 1 : 0;
 }
